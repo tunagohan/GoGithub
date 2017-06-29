29bbce79825df9b889ec1e2d8849f19b65071ee6
k-waragai <k-waragai@dip-net.co.jp>
2017-06-29 17:45:01 +0900

    リポジトリ一覧の表示

---
+++ RepoList.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"context"
+
+	"log"
+
+	"fmt"
+
+	"github.com/google/go-github/github"
+	"golang.org/x/oauth2"
+)
+
+func main() {
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: "{...Your Access Token...}"},
+	)
+	tc := oauth2.NewClient(ctx, ts)
+
+	client := github.NewClient(tc)
+
+	// list all repositories for the authenticated user
+	repos, _, err := client.Repositories.List(ctx, "{...UserName...}", nil)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	fmt.Println(repos)
+}
